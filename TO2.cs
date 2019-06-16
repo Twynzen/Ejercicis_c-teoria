1faee8fbb6eb4a1cbfacfc5ffac99b77dfa3c88a
Raziel <razzziel001@gmail.com>
2019-06-15 23:04:25 -0500

    Ejercicios simples c# en unity

---
+++ TO2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TO2 : MonoBehaviour {
+
+	// Depurar codigo
+	void Start () {
+        Debug.Log("num 1 ");
+        Debug.Log("num 2 ");
+        Debug.Log("num 3 ");
+        Debug.Log("num 4 ");
+        Debug.Log("num 5 ");
+
+    }
+	
+	
+}
